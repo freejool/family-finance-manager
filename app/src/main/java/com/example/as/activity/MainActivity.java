0a5660a61772bebea9162a3278bd1364f5b69a16
SuperSpecialSweet <sxy1066740232@gmail.com>
2022-05-29 15:55:42 +0800

    added mainactivity module

--- app/src/main/java/com/example/as/activity/MainActivity.java
+++ app/src/main/java/com/example/as/activity/MainActivity.java
@@ -10,6 +10,7 @@ import android.widget.ListView;
 
 import androidx.fragment.app.FragmentActivity;
 
+import com.example.as.Entity.TransType;
 import com.example.as.R;
 
 
@@ -17,7 +18,7 @@ public class MainActivity extends FragmentActivity {
     ListView gvInfo;// 创建GridView对象
     // 定义字符串数组，存储系统功能
     String[] titles = new String[]{"新增支出", "新增收入", "我的支出", "我的收入",
-            "数据管理", "系统设置", "收支便签", "帮助", "退出"};
+            "收支分析", "系统设置", "投资收益", "帮助", "退出", "收支类型"};
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -25,7 +26,7 @@ public class MainActivity extends FragmentActivity {
         setContentView(R.layout.main);
 
         gvInfo = findViewById(R.id.gv_info);// 获取布局文件中的gvInfo组件
-        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1,titles);// 创建pictureAdapter对象
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, titles);// 创建pictureAdapter对象
         gvInfo.setAdapter(adapter);// 为GridView设置数据源
         gvInfo.setOnItemClickListener(new OnItemClickListener() {// 为GridView设置项单击事件
             @Override
@@ -57,8 +58,9 @@ public class MainActivity extends FragmentActivity {
                         startActivity(intent);// 打开Sysset
                         break;
                     case 6:
-                        intent = new Intent(MainActivity.this, Accountflag.class);// 使用Accountflag窗口初始化Intent
-                        startActivity(intent);// 打开Accountflag
+                        // todo 主界面"投资收益"，进入后上方显示当前股票基金收益列表，下方一个按钮，通向投资建议。
+                        intent = new Intent(MainActivity.this, Profit.class);// 使用Profit窗口初始化Intent
+                        startActivity(intent);// 打开Profit
                         break;
                     case 7:
                         intent = new Intent(MainActivity.this, Help.class);// 使用Help窗口初始化Intent
@@ -66,6 +68,11 @@ public class MainActivity extends FragmentActivity {
                         break;
                     case 8:
                         finish();// 关闭当前Activity
+                        break;
+                    case 9:
+                        intent = new Intent(MainActivity.this, TransTypeModify.class);// 使用Help窗口初始化Intent
+                        startActivity(intent);// 打开Help
+                        break;
                 }
             }
         });
